dd4f603b32ee19ec5ad7983ea4bc96eea00d32c0
jinhyun <kyoulho@gmail.com>
2024-01-04 16:20:42 +0900

    programmers_42577

--- src/programmers/42577/kyoulho.java
+++ src/programmers/42577/kyoulho.java
@@ -1,16 +1,14 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
-    public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book, Comparator.comparingInt(String::length));
+public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
 
-        for (int i = 1; i < phone_book.length; i++) {
-            if (phone_book[i].startsWith(phone_book[0])) {
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
                 return false;
             }
         }
-
         return true;
     }
 }
