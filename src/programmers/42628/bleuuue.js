03b8fd34aa28ec6255da80cc8707385c12ec87a9
bleuuue <bleuuue@gmail.com>
2024-01-30 16:12:32 +0900

    programmers_42628

---
+++ src/programmers/42628/bleuuue.js
@@ -0,0 +1,23 @@
+function solution(operations) {
+  const queue = [];
+  for (let i = 0, len = operations.length; i < len; i++) {
+    if (operations[i].includes("I")) {
+      queue.push(Number(operations[i].substr(2)));
+    } else {
+      if (!queue.length) continue;
+
+      const findValue = (operations[i].includes("-") ? Math.min : Math.max)(
+        ...queue
+      );
+      const deleteIndex = queue.findIndex((val) => val === findValue);
+
+      queue.splice(deleteIndex, 1);
+    }
+  }
+
+  const answer = queue?.length
+    ? [Math.max(...queue), Math.min(...queue)]
+    : [0, 0];
+
+  return answer;
+}
