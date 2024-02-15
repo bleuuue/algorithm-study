4bb8ad791b7934adda0c99717a014517f21e12ae
bleuuue <bleuuue@gmail.com>
2024-02-15 19:45:20 +0900

    programmers_87946

--- src/programmers/87946/bleuuue.js
+++ src/programmers/87946/bleuuue.js
@@ -1,17 +1,16 @@
 function solution(k, dungeons) {
   let answer = 0;
   const visit = dungeons.map(() => false);
-  const dfs = (k, count, visit) => {
+    
+  const dfs = (k, count) => {
     for (let i = 0; i < dungeons.length; i++) {
       if (!visit[i] && k >= dungeons[i][0]) {
         visit[i] = true;
-        k -= dungeons[i][1];
-        answer = Math.max(answer, count);
-        dfs(k, count + 1, visit);
+        dfs(k - dungeons[i][1], count + 1);
+        visit[i] = false;
       }
-      visit[i] = false;
-      k += dungeons[i][1];
     }
+    answer = Math.max(answer, count);
   };
 
   dfs(k, 0, visit);
